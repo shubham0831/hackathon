4704909f217d586002241b8040abd441fa17d77b
Shubham Pareek <spareek@usfca.edu>
2022-10-08 15:51:53 -0700

    progress in replication

--- src/components/Charts/BarChart.svelte
+++ src/components/Charts/BarChart.svelte
@@ -2,39 +2,38 @@
   import { onMount } from "svelte";
   import Chart from "chart.js";
 
-  const chartData = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [4, 6, 10, 12, 15, 19],
-        backgroundColor: "rgba(3,117,220)",
-        indexAxis: 'y'
-      }
-    ]
-  };
-  const chartOptions = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
-        }
-      ]
-    }
-  };
+  onMount(async () => {
+    var ctx = document.getElementById(chartId);
+    fetch("some api")
+    .then(response => response.json)
+    .then((received) => {
+        chartData['labels'] = received.map((received) => received.bldg_name)
+        chartData['dataset'] = received.map((received) => received.consumption)
+        chartData['borderWidth'] = 2
+        chartData['borderColor'] = "rgb(3,117,220)"
+        chartData['backgroundColor'] = "rgba(3,117,220,0.2)"
 
-  function createChart() {
-    var ctx = document.getElementById("barChart");
-    var myChart = new Chart(ctx, {
-      type: "bar",
-      data: chartData,
-      options: chartOptions
-    });
-  }
+        const chartOptions = {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }
+              ]
+            }
+        };
+        
+        var myChart = new Chart(ctx, {
+          type: "line",
+          data: chartData,
+          options: chartOptions
+        });
+      })   
+  });
+  
 
-  onMount(createChart);
 </script>
 
 <canvas id="barChart" />
